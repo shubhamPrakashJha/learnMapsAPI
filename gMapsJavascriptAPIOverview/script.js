9387d58d63127e463eff06b25c39e354c4067c0d
shubhamPrakashJha <prakash.shubhamjha@gmail.com>
2018-08-01 15:27:07 +0530

    adding First marker in the Map

--- gMapsJavascriptAPIOverview/script.js
+++ gMapsJavascriptAPIOverview/script.js
@@ -1,11 +1,19 @@
 var map;
 
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'),{
+    map = new google.maps.Map(document.getElementById('map'), {
         center: {
             lat: 40.7413549,
             lng: -73.9980244
         },
         zoom: 13
+    });
+    var tribeca = {
+        lat: 40.719526,
+        lng: -74.0089934
+    };
+    var marker = new google.maps.Marker({
+        position: tribeca,
+        map: map
     });
 }
